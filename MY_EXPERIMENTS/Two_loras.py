9180f50e89e1280bae4a24b414f6743c29f5a336
Shucheng Li <lsc1885@gmail.com>
2026-01-26 20:02:38 +0000

    removed secrets

--- MY_EXPERIMENTS/Two_loras.py
+++ MY_EXPERIMENTS/Two_loras.py
@@ -5,10 +5,19 @@ from diffusers.utils import is_peft_available
 from diffusers import DiffusionPipeline
 
 
-os.environ["HF_TOKEN"] = "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"
+token_file = os.path.join(os.path.dirname(__file__), "HF_token.txt")
+if os.path.exists(token_file):
+    with open(token_file, "r") as f:
+        os.environ["HF_TOKEN"] = f.read().strip()
+if not os.environ.get("HF_TOKEN"):
+    raise ValueError("HF_TOKEN unset! Please ensure the local file 'HF_token.txt' contains the token.")
+print("✅ HF_TOKEN successfully loaded from local file.")
+
 current_dir = os.path.dirname(os.path.abspath(__file__))
 output_dir = os.path.join(current_dir, "outputs")
 os.makedirs(output_dir, exist_ok=True)
+
+
 
 print(f"Is PEFT available? {is_peft_available()}")
 # This MUST return True for load_lora_weights(adapter_name=...) to work
